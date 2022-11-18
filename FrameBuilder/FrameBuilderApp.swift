4db5aff158dd0fc848b8e3926c94c3bd4a4f418b
rebeloper <therebeloper@gmail.com>
2022-11-18 11:11:24 +0200

    Initial Commit

---
+++ FrameBuilder/FrameBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameBuilderApp.swift
+//  FrameBuilder
+//
+//  Created by Alex Nagy on 18.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct FrameBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
