59e4b4efcd7a5435d7049975c0a1c5a47d3005fb
rebeloper <therebeloper@gmail.com>
2022-11-18 11:25:45 +0200

    removed !

--- FrameBuilder/ContentView.swift
+++ FrameBuilder/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, GitHub dev!")
+            Text("Hello, GitHub dev")
         }
         .padding()
     }
